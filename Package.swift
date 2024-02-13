130c46c2a7d2c2f6d33ffc2417694b33965e12ea
Jack Leow <jackgene@gmail.com>
2024-02-13 08:50:52 -0800

    Use latest version of Curses that doesn't require dynamic linking.

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .package(url: "https://github.com/adorkable/swift-log-format-and-pipe.git", exact: "0.1.1"),
         .package(url: "https://github.com/apple/swift-argument-parser", exact: "1.2.2"),
         .package(url: "https://github.com/apple/swift-log.git", exact: "1.5.2"),
-        .package(url: "https://github.com/jackgene/Curses.git", exact: "1.0.2"),
+        .package(url: "https://github.com/jackgene/Curses.git", exact: "1.0.4"),
         .package(url: "https://github.com/ReactiveX/RxSwift.git", exact: "6.6.0"),
     ],
     targets: [
