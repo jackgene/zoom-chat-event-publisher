d98eac66ee989c04387e057881f65eec8686e9bd
Jack Leow <jackgene@gmail.com>
2024-07-14 08:53:39 -0700

    Added empty App target.

---
+++ App/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  App
+//
+//  Created by Jack Leow on 7/14/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
